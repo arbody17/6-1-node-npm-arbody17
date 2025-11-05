527555e4abe71eb1f44770d53e0654a95953409d
arbody <arbody.arb@gmail.com>
2025-11-05 21:30:31 +0300

    Task 1

--- calculator.js
+++ calculator.js
@@ -174,3 +174,6 @@ After completing all TODOs, test your calculator:
   Expected output: Invalid operation. Use: add, subtract, multiply, or divide
 
 */
+import { add, subtract, multiply, divide } from "./utils/operations.js";
+import { parseNumbers, isValidOperation } from "./utils/parser.js";
+import _ from "lodash";
